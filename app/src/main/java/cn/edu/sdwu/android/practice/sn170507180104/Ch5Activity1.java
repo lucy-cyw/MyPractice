d25ec0d5d1df0f07e60da493b4f203f4853687a8
lucy-cyw <15253783673@163.com>
2020-04-08 14:45:11 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/sdwu/android/practice/sn170507180104/Ch5Activity1.java
@@ -0,0 +1,90 @@
+package cn.edu.sdwu.android.practice.sn170507180104;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Ch5Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_ch5_1);
+    }
+    public void toast1(View view) {
+        Toast.makeText(this,"toast1",Toast.LENGTH_LONG).show();
+    }
+    public void toast2(View view){
+        Toast toast=Toast.makeText(this,"toast2",Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+    }
+    public void toast3(View view){
+        //加载自定义的布局文件，将其转化为View的实例
+        LayoutInflater layoutInflater=getLayoutInflater();
+        View view1=layoutInflater.inflate(R.layout.layout_toast,null);
+        //设置文本
+        TextView textView= (TextView) view1.findViewById(R.id.toast_tv);
+        textView.setText("toast3");
+        Toast toast=new Toast(this);
+        toast.setView(view1);//设置界面
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+    }
+
+    public void notification(View view){
+        Notification.Builder builder=new Notification.Builder(this);
+        //在构造器中，完成通知各个属性的设置
+        builder.setContentTitle("title");
+        builder.setContentText("message");
+        builder.setWhen(System.currentTimeMillis());
+        builder.setSmallIcon(android.R.drawable.ic_input_delete);//引用系统的资源
+        //调用build方法完成构造
+        Notification notification=builder.build();
+        //使用notificationManager,发送通知
+        NotificationManager notificationManager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(101,notification);
+    }
+    public void  cancel_notification(View view){
+        //使用NotificationManager取消通知
+        NotificationManager notificationManager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(101);
+    }
+    public void alert_dialog(View view){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        //在构造器中设置对话框的属性
+        builder.setTitle("title");
+        builder.setMessage("message");
+        builder.setIcon(android.R.drawable.ic_dialog_email);
+        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //添加点击按键后的处理
+                Toast.makeText(Ch5Activity1.this,"ok",Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNegativeButton("exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Ch5Activity1.this,"exit",Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNeutralButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Ch5Activity1.this,"cancel",Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.create().show();
+    }
+}
